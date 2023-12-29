ce50a8da76786db8e2bda0419c041e67385cdf33
Ariel Araújo <142962703+DevIniciante@users.noreply.github.com>
2023-12-28 22:54:33 -0300

    Cores
    Adicionando cores, ainda não está 100%

--- Baralho.py
+++ Baralho.py
@@ -1,5 +1,14 @@
 from random import shuffle
 
+c = ('\033[m',             #0- SEM CORES
+     '\033[0;30;41m',      #1- Vermelho
+     '\033[0;30;42m',      #2- VERDE
+     '\033[0;30;43m',      #3- AMARELO
+     '\033[0;30;44m',      #4- AZUL
+     '\033[0;30;45m',      #5- ROXO
+     '\033[7;30m'          #6- BRANCO
+    );
+
 class Card:
     suits = ["Espadas", "Coração", "Diamantes", "Clubes"]
     values = [None, None, "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
@@ -7,13 +16,13 @@ class Card:
     def __init__(self, v, s):
         """Suit + values são números inteiros"""
         self.value = v
-        self.suits = s
+        self.suit = s
 
     def __lt__(self, c2):
         if self.value < c2.value:
             return True
         if self.value == c2.value:
-            if self.suits < c2.suits:
+            if self.suit < c2.suit:
                 return True
             else:
                 return False
@@ -23,14 +32,14 @@ class Card:
         if self.value > c2.value:
             return True
         if self.value == c2.value:
-            if self.suits > c2.suits:
+            if self.suit > c2.suit:
                 return True
             else:
                 return False
         return False
 
     def __repr__(self):
-        v = Card.values[self.value] + " de " + Card.suits[self.suits]
+        v = Card.values[self.value] + " de " + Card.suits[self.suit]
         return v
 
 class Deck:
@@ -54,27 +63,27 @@ class Player:
 
 class Game:
     def __init__(self):
-        name1 = input("Nome do player 1: ")
-        name2 = input("Nome do player 2: ")
+        name1 = input(c[2], 'Nome do player 1: ', c[2])
+        name2 = input(c[2], 'Nome do player 2: ', c[2])
         self.deck = Deck()
         self.p1 = Player(name1)
         self.p2 = Player(name2)
 
     def print_winner(self, winner):
-        w = "{} Ganhou o round"
-        w = w.format(winner)
+        w = (c[4], '{} Ganhou o round', c[4])
+        w = w.format(winner.name)
         print(w)
 
     def draw(self, p1n, p1c, p2n, p2c):
-        d = "{} jogou {} e {} jogou {}"
+        d = (c[5], '{} jogou {} e {} jogou {}', c[0])  # Alterado para c[0] para evitar formatação de cor
         d = d.format(p1n, p1c, p2n, p2c)
         print(d)
 
     def play_game(self):
         cards = self.deck.cards
-        print("Começando a Guerra!!")
+        print('Começando a Guerra!!')
         while len(cards) >= 2:
-            m = "Pressione Q para sair. Qualquer outra tecla para jogar."
+            m = (c[6], 'Pressione Q para sair. Qualquer outra tecla para jogar.', c[6])
             response = input(m)
             if response.lower() == 'q':
                 break
@@ -85,20 +94,20 @@ class Game:
             self.draw(p1n, p1c, p2n, p2c)
             if p1c > p2c:
                 self.p1.wins += 1
-                self.print_winner(self.p1.name)
+                self.print_winner(self.p1)
             else:
                 self.p2.wins += 1
-                self.print_winner(self.p2.name)
+                self.print_winner(self.p2)
 
         win = self.winner(self.p1, self.p2)
-        print("A guerra acabou. {} wins".format(win))
+        print(c[1], 'A guerra acabou. {} ganhou'.format(win))  # Alterado de 'A guerra acabou. {} wins',c[1].format(win) para 'A guerra acabou. {} ganhou'.format(win)
 
     def winner(self, p1, p2):
         if p1.wins > p2.wins:
             return p1.name
         if p1.wins < p2.wins:
             return p2.name
-        return "Houve um empate"
+        return (c[1], 'Houve um empate', c[1])
 
 game = Game()
 game.play_game()
